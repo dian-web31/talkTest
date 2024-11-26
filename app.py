523b1728f3f802c6d250ea645723f9dfdbf15a54
dian-web31 <juan.henao36071@tools.ucaldas.edu.co>
2024-11-26 15:18:20 -0500

    interface (gradio)

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 import speech_recognition as sp
 from src import oracle_db as db
+import gradio as gr
 
-def speech_to_text():
-    rec = sp.Recognizer()
+def transcribe(audio):
+    recognizer = sp.Recognizer()
+    with sp.AudioFile(audio) as source:
+        audio_data = recognizer.record(source)
+        text = recognizer.recognize_google(audio_data, language="es-CO")
+        db.insert_row(text)
+    return text
 
-    my_micro = sp.Microphone(device_index=1)
-    on_air = True
-
-    while on_air:
-        try:
-            with my_micro as source:
-                print("say something...")
-                audio = rec.listen(source)
-                to_text = rec.recognize_google(audio, language="es-CO")
-                print("You said: {}".format(to_text))
-                
-                if to_text == "salir":
-                    on_air = False
-                else:
-                    db.insert_row(to_text)
-
-        except Exception as e:
-            print("Error: {}".format(e))
-
-if __name__ == "__main__":
-    speech_to_text()
+iface = gr.Interface(
+    fn=transcribe,
+    inputs=gr.Audio(sources="microphone"),
+    outputs="text",
+    live=True,
+    streaming=False
+)
+iface.launch()
